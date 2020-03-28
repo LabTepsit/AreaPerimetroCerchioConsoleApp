f738dba136d6023be0c396d288a8a4debed62f01
labinfo <labinfo4@secondarieclago.it>
2020-03-28 09:18:44 +0100

    Aggiungere i file di progetto.

---
+++ WpfAreaPerimetroCerchio/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAreaPerimetroCerchio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAreaAndPerimeter_Click(object sender, RoutedEventArgs e)
+        {
+            double risArea = 0;
+            //double risPerimeter = 0;
+
+            double raggio = Convert.ToDouble(txtRadius.Text);
+
+            HttpClient client = new HttpClient();
+
+            //https://localhost:44327/api/Operations/GetArea?raggio=1
+            string urlArea = $"https://localhost:44327/api/Operations/GetArea?raggio={raggio}";
+            //string urlPerimeter = "https://localhost:44327/api/Operations/GetPerimeter?raggio={raggio}";
+
+            HttpResponseMessage responseArea = await client.GetAsync(urlArea);
+            //HttpResponseMessage responsePerimeter = await client.GetAsync(urlPerimeter);
+
+            string contentArea = await responseArea.Content.ReadAsStringAsync();
+            //string contentPerimeter = await responsePerimeter.Content.ReadAsStringAsync();
+
+            risArea = JsonConvert.DeserializeObject<double>(contentArea);
+            //risPerimeter = JsonConvert.DeserializeObject<double>(contentPerimeter);
+
+            Dispatcher.Invoke(() => lblArea.Content = risArea);
+            //Dispatcher.Invoke(() => lblPerimeter.Content = risPerimeter);
+        }
+    }
+}
